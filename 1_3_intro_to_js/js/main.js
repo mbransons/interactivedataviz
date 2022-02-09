f3b7ac3fb697b1ae4b0a36a367475cd8be90d3d8
Michael Branson Smith <michaelbransonsmith@gmail.com>
2022-02-09 00:02:47 -0500

    add color changes with timer progression

--- 1_3_intro_to_js/js/main.js
+++ 1_3_intro_to_js/js/main.js
@@ -2,6 +2,7 @@
 gsap.registerPlugin(Draggable, InertiaPlugin);
 
 // selectors
+const gameTitle = document.querySelector('#game-title');
 const gameSpace = document.querySelector('.gamespace');
 const basket = document.querySelector('.basket');
 const timerDisplay = document.querySelector('#timer');
@@ -10,6 +11,9 @@ const startBtn = document.querySelector('#start');
 
 // used to global scope setInterval, timeOut values
 let startTimer, runTimerDisplay, startAddingCircles;
+
+// to allow game color changes with time
+let color = '#48c78e';
 
 // game data
 const gameState = {
@@ -46,7 +50,8 @@ function elPos() {
 // create div with circle class and set postition
 function makeCircle() {
   let circle = document.createElement('div');
-  circle.classList.add('circle', 'box', 'has-background-success');
+  circle.classList.add('circle', 'box');
+  circle.style.backgroundColor = color;
   let pos = elPos();
   let top, left;
   if (Math.random() < 0.5) {
@@ -96,7 +101,7 @@ function basketAnim() {
   tl.to(basket, {
     duration: 0.15,
     scale: 1.05,
-    backgroundColor: '#23d160',
+    backgroundColor: color,
   }).to(
     basket,
     { duration: 0.05, scale: 1, backgroundColor: '#b5b5b5' },
@@ -134,6 +139,13 @@ function resetGame() {
   gameState.draggableArr = [];
   timer = gameState.time;
   timerDisplay.value = 100;
+  timerDisplay.classList.remove('is-danger');
+  timerDisplay.classList.add('is-success');
+  gameTitle.classList.remove('has-text-danger');
+  gameTitle.classList.add('has-text-success');
+  startBtn.classList.remove('is-danger');
+  startBtn.classList.add('is-success');
+  color = '#48c78e';
   let circles = document.querySelectorAll('.circle');
   for (let circle of circles) {
     circle.remove();
@@ -144,9 +156,34 @@ function resetGame() {
 // display value range 100 to 0
 // a timer unit from gameState adjusts the timer decrement
 // also used to adjust the timer display value decrement
+// has color state changes as timerValue progresses
 function timerDiplayUpdate() {
   timer -= gameState.timerUnit();
-  timerDisplay.value = timer / (gameState.timerUnit() * 10);
+  let timerValue = timer / (gameState.timerUnit() * 10);
+  if (timerValue < 40 && timerValue > 15) {
+    gameTitle.classList.remove('has-text-success');
+    gameTitle.classList.add('has-text-warning');
+    startBtn.classList.remove('is-success');
+    startBtn.classList.add('is-warning');
+    timerDisplay.classList.remove('is-success');
+    timerDisplay.classList.add('is-warning');
+    color = '#ffe08a';
+    for (let circle of document.querySelectorAll('.circle')) {
+      circle.style.backgroundColor = color;
+    }
+  } else if (timerValue < 15) {
+    gameTitle.classList.remove('has-text-warning');
+    gameTitle.classList.add('has-text-danger');
+    startBtn.classList.remove('is-warning');
+    startBtn.classList.add('is-danger');
+    timerDisplay.classList.remove('is-warning');
+    timerDisplay.classList.add('is-danger');
+    color = '#f14668';
+    for (let circle of document.querySelectorAll('.circle')) {
+      circle.style.backgroundColor = color;
+    }
+  }
+  timerDisplay.value = timerValue;
 }
 
 // updates the counter display
