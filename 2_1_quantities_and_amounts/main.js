c1a74723d05d63812c34649ac6317889c558d0da
Michael Branson Smith <michaelbransonsmith@gmail.com>
2022-02-18 09:05:47 -0500

    add data path

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -2,13 +2,19 @@
 const width = window.innerWidth * 0.08;
 const height = window.innerHeight / 3;
 
+let squirrelData;
+
 /* LOAD DATA */
 // d3.autoType - detects datatypes and converts to the expected JS type
 // eg: '34' becomes 34
 d3.csv('../data/squirrelActivities.csv', d3.autoType).then((data) => {
+  squirrelData = data;
   console.log('data', data);
 
   /* SCALES */
+
+  // const xScale
+  // const yScale
   /** This is where you should define your scales from data to pixel space */
 
   /* HTML ELEMENTS */
