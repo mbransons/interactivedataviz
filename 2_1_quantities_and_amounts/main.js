8f4cd9c577d67f04cfebe96d0c4a389dadb5ac73
Michael Branson Smith <michaelbransonsmith@gmail.com>
2022-02-23 20:40:52 -0500

    build of charts from class

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,6 +1,21 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.08;
-const height = window.innerHeight / 3;
+const margin = { left: 80, right: 10, top: 100, bottom: 70 };
+const width = 600 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const svg = d3
+  .select('#chart-area')
+  .append('svg')
+  .attr(
+    'viewBox',
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  );
+
+const g = svg
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 let squirrelData;
 
@@ -11,11 +26,50 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType).then((data) => {
   squirrelData = data;
   console.log('data', data);
 
-  /* SCALES */
+  const squirrelActivities = squirrelData.map((d) => d.activity);
+  const color = d3
+    .scaleOrdinal()
+    .domain(squirrelActivities)
+    .range(d3.schemeCategory10);
 
-  // const xScale
-  // const yScale
-  /** This is where you should define your scales from data to pixel space */
+  /* SCALES */
+  const max = d3.max(data, (d) => d.count);
+  const x = d3
+    .scaleBand()
+    .domain(squirrelActivities)
+    .range([0, width])
+    .paddingInner(0.3)
+    .paddingOuter(0.2);
+
+  const y = d3.scaleLinear().domain([0, max]).range([height, 0]);
+
+  const rects = g.selectAll('rect').data(squirrelData);
+
+  rects
+    .enter()
+    .append('rect')
+    .attr('y', (d) => y(d.count))
+    .attr('x', (d) => x(d.activity))
+    .attr('width', x.bandwidth)
+    .attr('height', (d) => height - y(d.count))
+    .attr('fill', (d) => color(d.activity));
+
+  const xAxisCall = d3.axisBottom(x);
+  g.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height})`)
+    .call(xAxisCall)
+    .selectAll('text')
+    .attr('y', '10')
+    .attr('x', '-25')
+    .attr('text-achor', 'end')
+    .attr('transform', 'rotate(-50)');
+
+  const yAxisCall = d3
+    .axisLeft(y)
+    .ticks(10)
+    .tickFormat((d) => d);
+  g.append('g').attr('class', 'y axis').call(yAxisCall);
 
   /* HTML ELEMENTS */
   /** Select your container and append the visual elements to it */
--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,59 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const margin = { left: 80, right: 10, top: 100, bottom: 70 };
+const width = 600 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+const c = d3
+  .scaleOrdinal()
+  .domain(['R', 'D'])
+  .range(['#e41a1c', '#377eb8', '#ffff33']);
+
+const svg = d3
+  .select('#chart-area')
+  .append('svg')
+  .attr(
+    'viewBox',
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  );
+
+const g = svg
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 /* LOAD DATA */
-d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.json('../data/environmentRatings.json', d3.autoType).then((data) => {
+  console.log(data);
 
-    /* SCALES */
-    
-    /* HTML ELEMENTS */
-    
-  });
+  /* SCALES */
+  const x = d3.scaleLinear().domain([0, 100]).range([0, width]);
+  const y = d3.scaleLinear().domain([0, 1]).range([height, 0]);
+  /* HTML ELEMENTS */
+
+  const circles = g.selectAll('circle').data(data);
+  circles
+    .enter()
+    .append('circle')
+    .attr('class', 'circle')
+    .attr('fill', (d) => c(d.Party))
+    .attr('stroke', '#000000')
+    .attr('stroke-width', '1')
+    .attr('cx', (d) => x(d.envScore2020))
+    .attr('cy', (d) => y(d.ideologyScore2020))
+    .attr('r', 6);
+
+  const xAxisCall = d3
+    .axisBottom(x)
+    .ticks(10)
+    .tickFormat((d) => d);
+  g.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height})`)
+    .call(xAxisCall);
+
+  const yAxisCall = d3
+    .axisLeft(y)
+    .ticks(10)
+    .tickFormat((d) => d);
+  g.append('g').attr('class', 'y axis').call(yAxisCall);
+});
