4cf2991dea8135549a3bf7f2e81395efd4178cd1
Michael Branson Smith <michaelbransonsmith@gmail.com>
2022-03-04 11:41:43 -0500

    fix tooltip with data promise chaining

--- 2_2_distributions/movies.js
+++ 2_2_distributions/movies.js
@@ -87,8 +87,12 @@ async function search(movie) {
     .then((res) => res.data)
     .then((data) => data.results[0])
     .then((movie) => {
-      posterURL = `https://image.tmdb.org/t/p/w92${movie.poster_path}`;
+      return `https://image.tmdb.org/t/p/w92${movie.poster_path}`;
     });
+}
+
+async function setURL(movie) {
+  posterURL = await search(movie);
 }
 
 //ToolTip
@@ -100,7 +104,7 @@ const tip = d3
     <article class="media">
       <div class="media-left">
         <figure class="image">
-        <img src=${posterURL ? posterURL : 'img/no-poster-movie.png'}>
+        <img src=${d.posterURL}>
         </figure>
       </div>
       <div class="media-content">
@@ -133,64 +137,91 @@ const tip = d3
 g.call(tip);
 
 async function tipShow(event, d) {
-  await search(parseTitle(d[title]));
+  // await search(parseTitle(d[title]));
   tip.show(event, d);
 }
 
 let movieData;
 
-/* LOAD DATA */
-d3.csv('../data/highest-grossing-1000-movies.csv', d3.autoType).then((data) => {
-  movieData = data;
-  movieData.forEach((movie) => {
-    movie[date] = parseTime(movie[date]);
-    movie[genre] = parseGenres(movie[genre]);
-  });
-  // next date to add is 129
-  // removes movies without premiere date
-  const dataFix = movieData.filter((movie) => movie[date]);
-  console.log(dataFix);
-  /* SCALES */
-  //   const minDate = d3.min(dataFix, (d) => d[date]);
-  const minDate = parseTime('January 1, 1970');
-  const maxDate = d3.max(dataFix, (d) => d[date]);
-  const minSales = d3.min(dataFix, (d) => d[sales]);
-  const maxSales = d3.max(dataFix, (d) => d[sales]);
-  console.log(`
+let movieDataWithURLs;
+
+d3.csv('../data/highest-grossing-1000-movies.csv', d3.autoType)
+  .then((data) => {
+    data.forEach((movie) => {
+      movie[date] = parseTime(movie[date]);
+      movie[genre] = parseGenres(movie[genre]);
+      movie.posterURL = search(parseTitle(movie[title]));
+    });
+    return data;
+  })
+  .then((data) => {
+    return data.map((movie) => {
+      movie.posterURL.then((url) => {
+        movie.posterURL = url;
+      });
+      return movie;
+    });
+  })
+  .then((data) => {
+    const dataFix = data.filter((movie) => movie[date]);
+    console.log(dataFix);
+    /* SCALES */
+    //   const minDate = d3.min(dataFix, (d) => d[date]);
+    const minDate = parseTime('January 1, 1970');
+    const maxDate = d3.max(dataFix, (d) => d[date]);
+    const minSales = d3.min(dataFix, (d) => d[sales]);
+    const maxSales = d3.max(dataFix, (d) => d[sales]);
+    console.log(`
   minDate: ${minDate} 
   maxDate: ${maxDate}
   minSales: ${minSales}
   maxSales: ${maxSales}`);
-  const x = d3.scaleTime().domain([minDate, maxDate]).range([0, width]);
-  const y = d3
-    .scaleLog()
-    .domain([70000000, maxSales])
-    .range([height, 0])
-    .base(10);
+    const x = d3.scaleTime().domain([minDate, maxDate]).range([0, width]);
+    const y = d3
+      .scaleLog()
+      .domain([70000000, maxSales])
+      .range([height, 0])
+      .base(10);
 
-  const xAxisCall = d3.axisBottom(x).ticks(10).tickFormat(d3.timeFormat('%Y'));
-  g.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', `translate(0, ${height})`)
-    .call(xAxisCall);
+    const xAxisCall = d3
+      .axisBottom(x)
+      .ticks(10)
+      .tickFormat(d3.timeFormat('%Y'));
+    g.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxisCall);
 
-  const yAxisCall = d3
-    .axisLeft(y)
-    .ticks(10)
-    .tickFormat((d) => d3.format('$,.3s')(d).replace(/G/, 'B'));
-  g.append('g').attr('class', 'y axis').call(yAxisCall);
+    const yAxisCall = d3
+      .axisLeft(y)
+      .ticks(10)
+      .tickFormat((d) => d3.format('$,.3s')(d).replace(/G/, 'B'));
+    g.append('g').attr('class', 'y axis').call(yAxisCall);
 
-  const circles = g.selectAll('circle').data(dataFix);
-  circles
-    .enter()
-    .append('circle')
-    .attr('class', 'circle')
-    .attr('fill', '#ffff33')
-    .attr('stroke', '#000000')
-    .attr('stroke-width', '1')
-    .on('mouseover', (e, d) => tipShow(e, d))
-    .on('mouseout', tip.hide)
-    .attr('cx', (d) => x(d[date]))
-    .attr('cy', (d) => y(d[sales]))
-    .attr('r', 6);
-});
+    const circles = g.selectAll('circle').data(dataFix);
+    circles
+      .enter()
+      .append('circle')
+      .attr('class', 'circle')
+      .attr('fill', '#ffff33')
+      .attr('stroke', '#000000')
+      .attr('stroke-width', '1')
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
+      .attr('cx', (d) => x(d[date]))
+      .attr('cy', (d) => y(d[sales]))
+      .attr('r', 6);
+  });
+
+/* LOAD DATA */
+// d3.csv('../data/highest-grossing-1000-movies.csv', d3.autoType).then((data) => {
+//   movieData = data;
+//   movieData.forEach((movie) => {
+//     movie[date] = parseTime(movie[date]);
+//     movie[genre] = parseGenres(movie[genre]);
+//     movie.posterURL = search(parseTitle(movie[title]));
+//   });
+//   // next date to add is 129
+//   // removes movies without premiere date
+
+// });
