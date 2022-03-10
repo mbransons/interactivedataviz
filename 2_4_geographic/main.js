d4c010d4ff0225c05c92588a154aba28a90d1184
Michael Branson Smith <michaelbransonsmith@gmail.com>
2022-03-09 20:01:24 -0500

    start geographic demo

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,26 +1,40 @@
-/* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+// set margins, width/height
+const margin = { left: 100, right: 20, top: 100, bottom: 50 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+// use viewBox rather than x and y values so that a aspect ratio is set and the visualization can be responsively scaled
+const svg = d3
+  .select('#chart-area')
+  .append('svg')
+  .attr(
+    'viewBox',
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  );
+
+// Visualization
+// offset a group based on margins so that height and width can be used when building scales
+const g = svg
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+//Scales
+// Initially set ranges based on the visualization width/height
+const projection = d3.geoAlbersUsa();
 
 /**
  * LOAD DATA
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("../data/usState.json"),
-  d3.csv("../data/stateCapitals.csv", d3.autoType),
+  d3.json('../data/usState.json'),
+  d3.csv('../data/stateCapitals.csv', d3.autoType),
 ]).then(([geojson, capitals]) => {
-  
   // SPECIFY PROJECTION
- 
-
+  projection.fitSize([width, height], geojson);
   // DEFINE PATH FUNCTION
-
-
-  // APPEND GEOJSON PATH  
-  
-  
+  // APPEND GEOJSON PATH
   // APPEND DATA AS SHAPE
-
 });
