e73a2fe5e9530f99e7a23d59968439365bd2d6bd
Michael Branson Smith <michaelbransonsmith@gmail.com>
2022-03-22 12:30:05 -0400

    add more notes to HTML and comments to JS

--- 2_3_time_series/internet.js
+++ 2_3_time_series/internet.js
@@ -3,9 +3,6 @@ const margin = { left: 90, right: 305, top: 15, bottom: 40 };
 const width = 1780 - margin.left - margin.right;
 const height = 1570 - margin.top - margin.bottom;
 
-// declare data variable to assign value after data call
-let data;
-
 // use viewBox rather than x and y values so that a aspect ratio is set and the visualization can be responsively scaled
 const svg = d3
   .select('#chart-area')
@@ -23,14 +20,8 @@ const g = svg
   .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// Labels
-// Legend Group bottom-right
-const legend = g
-  .append('g')
-  .attr('transform', `translate(${width - 10}, ${height - 150})`);
-
+// Arrays of values from data set to help build
 const regionCodes = ['NAC', 'ECS', 'LCN', 'EAS', 'MEA', 'WLD', 'SAS', 'SSF'];
-
 const regionNames = [
   'North America',
   'Europe & Central Asia',
@@ -49,7 +40,7 @@ const formatTime = d3.timeFormat('%Y');
 const formatPop = d3.format('.3s');
 
 //Scales
-// Initially set ranges based on the visualization width/height
+// Given this simplicity of the domains, both ranges and domains are set without data
 const startDate = d3.timeDay.offset(parseYear(1990), -80);
 const endDate = d3.timeDay.offset(parseYear(2016), 80);
 const x = d3.scaleTime().range([0, width]).domain([startDate, endDate]);
@@ -74,21 +65,22 @@ const line = d3
   .x((d) => x(d.year))
   .y((d) => y(d.percent));
 
+// X Axis
 const xAxisCall = d3
   .axisBottom(x)
   .ticks(30)
   .tickFormat((d) => formatTime(d));
 
-//Y Axis
-const yAxisCall = d3
-  .axisLeft(y)
-  .ticks(10)
-  .tickFormat((d) => `${d}%`);
-
 g.append('g')
   .attr('class', 'x axis')
   .attr('transform', `translate(0, ${height})`)
   .call(xAxisCall);
+
+// Y Axis
+const yAxisCall = d3
+  .axisLeft(y)
+  .ticks(10)
+  .tickFormat((d) => `${d}%`);
 
 g.append('g').attr('class', 'y axis').call(yAxisCall);
 
@@ -106,7 +98,7 @@ function make_y_gridlines() {
 // add the X gridlines
 g.append('g')
   .attr('class', 'grid grid-x')
-  .attr('transform', 'translate(0,' + height + ')')
+  .attr('transform', `translate(0, ${height})`)
   .call(make_x_gridlines().tickSize(-height).tickFormat(''));
 
 // add the Y gridlines
@@ -114,6 +106,7 @@ g.append('g')
   .attr('class', 'grid grid-y')
   .call(make_y_gridlines().tickSize(-width).tickFormat(''));
 
+// variables declared in the global scope that are used after the data call
 let pop,
   meta,
   internet,
@@ -123,8 +116,7 @@ let pop,
   countriesLabelSort,
   regions;
 
-// Call Data
-// https://ourworldindata.org/internet
+// Call Data downloaded from
 // https://data.worldbank.org/indicator/it.net.user.zs
 // https://data.worldbank.org/indicator/SP.POP.TOTL
 Promise.all([
@@ -138,6 +130,8 @@ Promise.all([
   internet = i;
   meta = m;
 
+  // meta array is used to add values from the internet data (i) and population data (p)
+  // the country code 'ABC' is used to find values
   meta.forEach((c) => {
     let matchI = internet.find((i) => i['Country Code'] === c['Country Code']);
     let matchP = pop.find((p) => p['Country Code'] === c['Country Code']);
@@ -158,24 +152,24 @@ Promise.all([
     c.data = data;
   });
 
+  // With all values in the meta array, two filtered sets are created
+  // all countries have a region defined
+  // regions are picked out from the regionCodes
   countries = meta.filter((c) => c.Region);
   regions = meta.filter((r) =>
     regionCodes.some((code) => code === r['Country Code'])
   );
 
-  // North Korea final data point fix
-  countries
-    .find((c) => c['Country Code'] === 'PRK')
-    .data.push({ year: parseYear(2016), percent: 0 });
-
+  // A bit of data massaging here. For countries that do not have data all the way to 2016
+  // This adds a value to the year 2016 as a repeat of the last recorded value
+  // Prevents all these countries from having their final value kick down to 0% from a higher value
+  // Also removes any countries that do not have data
   countriesAdd = countries.map((c) => {
     if (c.data.some((val) => formatTime(val.year) === '2016')) {
       return c;
     } else {
       if (c.data.length > 0) {
         let last = c.data[c.data.length - 1];
-        console.log(last);
-        console.log(c.data);
         last.year = parseYear(2016);
         c.data.push(last);
         return c;
@@ -183,7 +177,22 @@ Promise.all([
       return c;
     }
   });
-  countriesLast = countries
+
+  // With work on the data completed, the countries paths are generated
+  g.selectAll('.country-path')
+    .data(countriesAdd)
+    .enter()
+    .append('path')
+    .attr('id', (d) => d['Country Code'])
+    .attr('class', 'country-path')
+    .attr('fill', 'none')
+    .attr('stroke', '#b3b3b3')
+    .attr('stroke-width', 2)
+    .attr('d', (d) => line(d.data));
+
+  // simplified array of data is built for managing the labels to be added
+  // after the final year 2016
+  countriesLast = countriesAdd
     .filter((c) => c.data.some((val) => formatTime(val.year) === '2016'))
     .map((c) => {
       return {
@@ -193,12 +202,29 @@ Promise.all([
       };
     });
 
-  let curr, obj;
-  // reduces data returning a new array of objects one per country
-  // sample object structure
-  // {country: name, data: [{year: date Obj, population: num}, {}, {}]}
+  // a group is created for countries and positioned in the SVG
+  // at the end of the country path line
+  const countryLabel = g
+    .selectAll('.country-label')
+    .data(countriesLast)
+    .enter()
+    .append('g')
+    .attr(
+      'transform',
+      (d) => `translate(${x(d.data.year)}, ${y(d.data.percent)})`
+    );
 
+  // circles are then added to the group which appear at the end of the country path line
+  countryLabel
+    .append('circle')
+    .attr('r', '3')
+    .attr('class', (d) => d['Country Code'])
+    .attr('fill', '#b3b3b3');
+
+  // then the data is sorted by percentage and then reduced to groupings
+  // based on rounded percentage number of users in 2016
   // countriesLabelSort
+  let curr, obj;
   countriesLabelSort = countriesLast
     .sort((a, b) => a.data.percent - b.data.percent)
     .reduce((acc, val) => {
@@ -217,33 +243,8 @@ Promise.all([
       return acc;
     }, []);
 
-  g.selectAll('.country-path')
-    .data(countries)
-    .enter()
-    .append('path')
-    .attr('id', (d) => d['Country Code'])
-    .attr('class', 'country-path')
-    .attr('fill', 'none')
-    .attr('stroke', '#b3b3b3')
-    .attr('stroke-width', 2)
-    .attr('d', (d) => line(d.data));
-
-  const countryLabel = g
-    .selectAll('.country-last')
-    .data(countriesLast)
-    .enter()
-    .append('g')
-    .attr(
-      'transform',
-      (d) => `translate(${x(d.data.year)}, ${y(d.data.percent)})`
-    );
-
-  countryLabel
-    .append('circle')
-    .attr('r', '3')
-    .attr('class', (d) => d['Country Code'])
-    .attr('fill', '#b3b3b3');
-
+  // The sorted country groups need a group to build the rounded percentage
+  // and the list of countries at that percent
   const countryLabelText = g
     .selectAll('.country-label')
     .data(countriesLabelSort)
@@ -253,6 +254,9 @@ Promise.all([
       'transform',
       (d) => `translate(${x(parseYear(2016))}, ${y(d.percent)})`
     );
+
+  // a text element is added to the group and a multiple function is used
+  // to allow for iteration through the array of countries at the percent
   countryLabelText
     .append('text')
     .attr('class', 'country-label')
@@ -261,6 +265,9 @@ Promise.all([
     .attr('fill', '#b3b3b3')
     .each(multiple);
 
+  // adds tspans for each value needed in the text element
+  // this allows for classes to be assigned to each tspan
+  // which can be used to select and modify based on the value
   function multiple(d) {
     let text = d3.select(this);
     text
@@ -272,6 +279,9 @@ Promise.all([
     });
   }
 
+  // using the filtered meta array into the regions array
+  // the regions paths are created using the color scale
+  // for path fills
   g.selectAll('.region')
     .data(regions)
     .enter()
@@ -281,6 +291,7 @@ Promise.all([
     .attr('stroke-width', 6)
     .attr('d', (d) => line(d.data));
 
+  // a group for the regions label is created at the end of the regions path line
   const regionsLabel = g
     .selectAll('.region-last')
     .data(regions)
@@ -293,10 +304,14 @@ Promise.all([
           d.data[d.data.length - 1].percent
         )})`
     );
+
+  // a circle is added to the regions label group
   regionsLabel
     .append('circle')
     .attr('r', '7')
     .attr('fill', (d) => regColor(d['Country Code']));
+
+  // the region name is added to the regions label group
   regionsLabel
     .append('text')
     .attr('class', 'region-label')
@@ -306,6 +321,15 @@ Promise.all([
     )
     .attr('fill', (d) => regColor(d['Country Code']))
     .attr('transform', 'translate(10, 5)');
+
+  // country path hover state interaction
+  // all paths are selected by the common class
+  // two event listeners 'mouseover' and 'mouseout' on the path
+  // The id attached to each path is its 'Country Code' so it
+  // can be used to look up country object in countries and use
+  // it's region to set the color of the path
+  // also the country code is the class of the circle and country
+  // in the country label to change their colors
   const countryPaths = document.querySelectorAll('.country-path');
   for (let path of countryPaths) {
     path.addEventListener('mouseover', function () {
@@ -337,12 +361,3 @@ Promise.all([
     });
   }
 });
-
-function join(arr) {
-  return arr.map((obj) => {
-    if (obj.name.indexOf(',') !== -1) {
-      return obj.name.slice(0, obj.name.indexOf(','));
-    }
-    return obj.name;
-  });
-}
